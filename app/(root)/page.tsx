1137ba3c4658afd29f9c78a290df9d383e48dede
commited_dev <devcommited@gmail.com>
2025-09-28 11:01:08 +0200

    feat(view): add branding variables

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-center text-5xl">Welcome to Fund Me App</h1>
+      <h1 className="text-center text-5xl text-gradient-brand">Welcome to Fund Me App</h1>
     </main>
   );
 }
