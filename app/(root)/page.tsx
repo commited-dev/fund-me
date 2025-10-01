af2f2da6807488bfa933afc5027562000e2f86bd
commited_dev <devcommited@gmail.com>
2025-10-01 09:32:06 +0200

    create a hero section with a search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import SearchForm from '@/components/search-form';
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
-    <main>
-      <h1 className="heading">Welcome to Fund Me App</h1>
-    </main>
+    <>
+      <section className="brand-container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and get noticed in Virtual Competitions
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+    </>
   );
 }
