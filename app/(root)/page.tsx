b53ed9e3f757a9bd3a20d5b1fb747ee20ff1e43d
commited_dev <devcommited@gmail.com>
2025-10-01 21:01:56 +0200

    add search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import SearchForm from '@/components/search-form';
 import StartupCard from '@/components/startup-card';
 import { STARTUPS_QUERY } from '@/lib/queries';
-import { client } from '@/sanity/lib/client';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 import { StartupCardType } from '@/types';
 
 export default async function Home({
@@ -10,8 +10,9 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -30,15 +31,17 @@ export default async function Home({
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : 'All Startups'}
         </p>
+
+        <ul className="mt-7 card-grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => <StartupCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
 
-      <ul className="mt-7 card-grid">
-        {posts?.length > 0 ? (
-          posts.map((post: StartupCardType) => <StartupCard key={post?._id} post={post} />)
-        ) : (
-          <p className="no-results">No startups found</p>
-        )}
-      </ul>
+      <SanityLive />
     </>
   );
 }
